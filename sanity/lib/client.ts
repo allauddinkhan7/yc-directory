2905696a176cf65e645f4a1c9f8b0d9bdf4f9209
allauddink7 <khanallauddin113@gmail.com>
2025-11-11 23:19:08 +0500

    gyuh

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,12 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation- without need to refresh the browser cache
 })
+
+//  useCdn: false, means we always want fresh data and not the cached version. This is useful when we are frequently updating content and want to see the changes immediately. this is SSG
+//  useCdn: true, means we want to use the cached version of the data. This is useful for public sites where content doesn't change frequently and we want to optimize for performance. this is not ISG because it serves cached data
+// ISR - Incremental Static Regeneration means new data is fetched and updated at regular intervals combo of ISR and SSR
+// SSG - Static Site Generation means data is fetched at build time and remains static until the next build
+
+// 
